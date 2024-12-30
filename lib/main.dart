0283ee86172626c7ff81cd38f8b2591b4151fd0e
PowChorba <101152834+PowChorba@users.noreply.github.com>
2024-12-30 16:16:19 -0300

    disclaimer page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:target_speed_737/src/presentation/pages/disclaimer/disclaimer_page.dart';
 import 'package:target_speed_737/src/presentation/pages/home/home_page.dart';
 import 'package:target_speed_737/src/presentation/pages/results/results_page.dart';
 import 'package:target_speed_737/src/themes/dark_mode.dart';
@@ -18,8 +19,9 @@ class MyApp extends StatelessWidget {
       title: 'target_speed_737',
       theme: darkMode,
       // home: HomePage(),
-      initialRoute: 'home',
+      initialRoute: 'disclaimer',
       routes: {
+        'disclaimer': (BuildContext context) => const DisclaimerPage(),
         'home': (BuildContext context) => const HomePage(),
         'results': (BuildContext context) => const ResultsPage()
       },
---
+++ lib/src/presentation/pages/disclaimer/disclaimer_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class DisclaimerPage extends StatefulWidget {
+  const DisclaimerPage({super.key});
+
+  @override
+  State<DisclaimerPage> createState() => _DisclaimerPageState();
+}
+
+class _DisclaimerPageState extends State<DisclaimerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          width: double.infinity,
+          color: Theme.of(context).colorScheme.surfaceContainer,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'WARNING',
+                style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.error),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              const Text(
+                'This product is for informational and educational purposes only.',
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                'Your FAA-Approved Airplane FLight Manual or its equivalent is the final authority as to the safe operation of your airplane.',
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                'Under no cricumstances should this application be used for flight operations.',
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, 'home', (route) => false);
+                },
+                child: Container(
+                  // color: Theme.of(context).colorScheme.onPrimary,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Theme.of(context).colorScheme.onPrimary),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 50),
+                  child: Text('I understand',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Theme.of(context).colorScheme.onSecondary)),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
